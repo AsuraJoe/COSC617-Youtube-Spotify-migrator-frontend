3ace6e33a370fee507cb97cf8f880ffffe83b50d
AsuraJoe <41365507+AsuraJoe@users.noreply.github.com>
2021-12-07 13:36:04 -0500

    added UI code

--- playlist-react-app/src/App.js
+++ playlist-react-app/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const url='http://localhost:4657/playlist/PLjyBSjRcT7fuAW9p-EZUQbctCEYUXJ15u';
 
 function App() {
+  const [songs, setSongs] = useState(null);
+
+  useEffect(()=>{
+    axios.get(`${url}`).then((response) => {
+      console.log(response.data);
+      setSongs(response.data);
+    })
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {!songs?null:songs.map(song=><Song song={ song}/>)}
+      </ul>
     </div>
   );
 }
 
+function Song(props){
+  const song=props.song
+  const embed=`https://www.youtube.com/embed/${song.id}`;
+  return (
+    <div className="song" class="card">
+      {/* <h2>{song.title}</h2> */}
+      <iframe src={embed} title="youtube" loading="lazy"></iframe>
+    </div>
+  );
+}
 export default App;
